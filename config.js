987b93509d778ce710b4e52a192c370a0cd470b2
KSVMODS <147960550+KSVMODS@users.noreply.github.com>
2023-11-02 18:06:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ global.owner = [
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
+global.mods = ['91760590201','32460248586'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
